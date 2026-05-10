7dcc900f19a8c570f768b3fd2b338b6dfa99aacb
jamilsalsabila <jamilsalsabila@gmail.com>
2026-05-10 22:29:03 +0700

    fix SVG start date

--- index.php
+++ index.php
@@ -573,7 +573,8 @@ tailwind.config = {
                                     </span>
                                     <span x-show="task.start_date" class="flex items-center gap-1">
                                         <svg class="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3"/>
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
                                         </svg>
                                         <span x-text="'Mulai: ' + formatDateFull(task.start_date)"></span>
                                     </span>
